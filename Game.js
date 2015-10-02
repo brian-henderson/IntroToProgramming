ef6757a0da3b23f162c75fc4e6f6d30785098c4a
Brian Henderson <brian.henderson1@marist.edu>
2015-10-01 21:35:13 -0400

    Added CSS and made the display prettier

--- Game.js
+++ Game.js
@@ -1,7 +1,7 @@
 var currentLocation = "beach";
 
 var messageBeach = "You are back on the beach."
-var messageJungle = "You are now in the jungle.";
+var messageJungle = "You are deep in the jungle right now. Maybe you should switch directions.";
 var messageCliffBridge = "You have reached a bridge that will take you across the cliff";
 var messageJungleDeadEnd = "You have reached a dead end as you are stopped by a cliff edge. Try turning around";
 var messageNorthStop = "You can not go north this way! Try a different direction.";
@@ -35,91 +35,73 @@ function North () {
 	if (currentLocation === "beach") {
 		currentLocation = "jungle";
 		showMessage(messageJungle);
-
 		if (scoreJungle === true) {
 			score += 5;
 			scoreJungle = false;
 			showScore(score);
 		}
-	
 	} else if (currentLocation === "cliff") {
 		currentLocation = "cliffBridge";
 		showMessage(messageCliffBridge);
-
 		if (scoreCliffBridge === true) {
 			score += 5;
 			scoreCliffBridge = false;
 			showScore(score);
 		}
-	
 	} else if (currentLocation === "archway") {
 		currentLocation = "jungleDeadEnd";
 		showMessage(messageJungleDeadEnd);
-	
 	} else {
 		showMessage(messageNorthStop);
 	}
 }	
-
 function East () {
 	if (currentLocation === "jungle") {
 		currentLocation = "cliff";
 		showMessage(messageCliff);
-
 		if (scoreCliff === true) {
 			score += 5;
 			scoreCliff = false;
 			showScore(score);
 		}
-
 	} else if (currentLocation ==="cliffBridge") {
 		currentLocation = "archway";
 		showMessage(messageArchway);
-
 		if (scoreArchway === true) {
 			score += 5;
 			scoreArchway = false;
 			showScore(score);
-		}
-	
+		}	
 	} else if (currentLocation === "jungleClearing") {
 		currentLocation = "temple";
 		showMessage(messageTemple);
-
 		if (scoreTemple === true) {
 			score += 5;
 			scoreTemple = false;
 			showScore(score);
 		}
-	
 	} else {
 		showMessage(messageEastStop);
 	}		
 }
-
 function South () {
 	if (currentLocation === "jungle") {
 		currentLocation = "beach";
-		showMessage(messageBeach);
-	
+		showMessage(messageBeach);	
 	} else if (currentLocation === "cliffBridge") {
 		currentLocation = "cliff";
 		showMessage(messageCliff2);
-
 	} else if (currentLocation === "jungleDeadEnd") {
 		currentLocation = "archway";
 		showMessage(messageArchway);
-	
 	} else if (currentLocation === "archway") {
 		currentLocation = "jungleClearing";
 		showMessage(messageClearing);
-
 		if (scoreJungleClearing === true) {
 			score += 5;
 			scoreJungleClearing = false;
 			showScore(score);
 		}
-	
 	} else {
 		showMessage(messageSouthStop);
 	}
@@ -129,11 +111,9 @@ function West () {
 	if (currentLocation === "cliff") {
 		currentLocation = "jungle";
 		showMessage(messageJungle);
-	
 	} else if (currentLocation === "archway") {
 		currentLocation = "cliffBridge";
 		showMessage(messageCliffBridge);
-		
 	} else {
 		showMessage(messageWestStop);
 	}
