06d557f516bf1f3cce69097601cd6133c9f8114e
Brian Henderson <brian.henderson1@marist.edu>
2015-10-01 22:19:09 -0400

    Made location messages better.

--- Game.js
+++ Game.js
@@ -2,16 +2,16 @@ var currentLocation = "beach";
 
 var messageBeach = "You are back on the beach."
 var messageJungle = "You are deep in the jungle right now. Maybe you should switch directions.";
-var messageCliffBridge = "You have reached a bridge that will take you across the cliff";
-var messageJungleDeadEnd = "You have reached a dead end as you are stopped by a cliff edge. Try turning around";
+var messageCliffBridge = "Ah you were right! You have reached an old rope bridge, but it looks old and dangerous. Looks like it is the only way across though.";
+var messageJungleDeadEnd = "You have reached a dead end as you are stopped by a waterfall. Try turning around.";
 var messageNorthStop = "You can not go north this way! Try a different direction.";
-var messageCliff = "Whoa! That was a close one! You almost fell off the edge of this cliff. Looks like a rope bridge might be a little north."
-var messageCliff2 = "You are back by the edge of the cliff from before.";
-var messageArchway = "Ya! You have safely crossed the bridge! There is a sign that points North and South, but you are unable to read the text.";
+var messageCliff = "Whoa! That was a close one! That cliff edge came out of no where! Looks like there might be a rope bridge a little north."
+var messageCliff2 = "You are back by the edge of the cliff from before. Try going north again towards the bridge. It may be the only option.";
+var messageArchway = "Yay! You have safely crossed the bridge! There is a sign that points North and South, but you are unable to interprete the ancient symbols.";
 var messageTemple = "You have reached the ancient ruins of the Temple of Aboubaker!";
 var messageEastStop = "You can not head east here! Try a different direction.";
-var messageClearing = "You have reached a clearing in the jungle. Wait. What is that? It looks like some sort of ancient structure off to the east.";
-var messageSouthStop = "You cant go south here. Try a different direction.";
+var messageClearing = "You have reached a clearing in the jungle. You begin to loose hope after hours of searc...Wait. Whats that off to the east?";
+var messageSouthStop = "You can't go south here. Try a different direction.";
 var messageWestStop = "Hrmm. Looks like going west is not an option. Try another direction.";
 
 var score = 0;
